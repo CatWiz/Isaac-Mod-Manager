d4cbd8b140e0d6bb3c80e5226e2c1cfc19ce5a3e
CatWizard <thecatwizard124@gmail.com>
2025-03-28 02:44:36 +0200

    enabled/disabled mods are always sorted

--- ModManager/ViewModels/MainWindowViewModel.cs
+++ ModManager/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
@@ -7,40 +8,49 @@ using System.Runtime.InteropServices.JavaScript;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using ModManager.Models;
+using Tmds.DBus.Protocol;
 
 namespace ModManager.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     private string _selectedPath = string.Empty;
+    private ObservableCollection<Mod> _enabledMods = new();
+    private ObservableCollection<Mod> _disabledMods = new();
+
     public string SelectedPath
     {
         get => string.IsNullOrEmpty(_selectedPath) ? "" : _selectedPath;
         set => SetProperty(ref _selectedPath, value);
     }
-    
-    public ObservableCollection<Mod> EnabledMods { get; private set; } = new();
-    public ObservableCollection<Mod> DisabledMods { get; private set; } = new();
+
+    public ObservableCollection<Mod> EnabledMods
+    {
+        get => _enabledMods;
+        private set
+        {
+            _enabledMods = new ObservableCollection<Mod>(value.OrderBy(m => m.FolderName));
+            OnPropertyChanged();
+        }
+    }
+
+    public ObservableCollection<Mod> DisabledMods
+    {
+        get => _disabledMods;
+        private set
+        {
+            _disabledMods = new ObservableCollection<Mod>(value.OrderBy(m => m.FolderName));
+            OnPropertyChanged();
+        }
+    }
 
     public void UpdateModsList(string basePath)
     {
         var modsFolderPath = Path.Combine(basePath, "mods");
         var mods = Mod.GetModsList(modsFolderPath);
-        
-        EnabledMods.Clear();
-        DisabledMods.Clear();
 
-        foreach (var mod in mods)
-        {
-            if (mod.Enabled)
-            {
-                EnabledMods.Add(mod);
-            }
-            else
-            {
-                DisabledMods.Add(mod);
-            }
-        }
+        EnabledMods = new ObservableCollection<Mod>(mods.Where(m => m.Enabled));
+        DisabledMods = new ObservableCollection<Mod>(mods.Where(m => !m.Enabled));
     }
     
     public void DisableMods(IEnumerable<Mod> mods)
@@ -51,6 +61,8 @@ public partial class MainWindowViewModel : ViewModelBase
             EnabledMods.Remove(mod);
             DisabledMods.Add(mod);
         }
+        // Trigger the OnPropertyChanged event
+        DisabledMods = DisabledMods;
     }
     
     public void EnableMods(IEnumerable<Mod> mods)
@@ -61,5 +73,7 @@ public partial class MainWindowViewModel : ViewModelBase
             DisabledMods.Remove(mod);
             EnabledMods.Add(mod);
         }
+        // Trigger the OnPropertyChanged event
+        EnabledMods = EnabledMods;
     }
 }
